d4f545552f2baeab5dd82efbe6471af1f312f5ee
BJX <1596311683@qq.com>
2017-05-10 15:15:54 +0800

    添加了一个查询实例

---
+++ SSMLearning/src/com/control/selectFromWorkOrder/WorkOrderController.java
@@ -0,0 +1,28 @@
+package com.control.selectFromWorkOrder;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dao.mapping.workOrder;
+import com.dao.mapping.res.workOrderMapper;
+
+@Controller
+@RequestMapping("/workOrder")
+public class WorkOrderController {
+	Logger logger = Logger.getLogger(WorkOrderController.class);
+	@Autowired
+	workOrderMapper WorkOrderMapper;
+	@RequestMapping(value="/test",method=RequestMethod.GET)
+	public @ResponseBody String Test(){
+		return "这是一个测试的首页，会不断地进行丰富";	
+	}
+	
+	@RequestMapping(value="/selectByPrimaryKey",method=RequestMethod.GET)
+	public @ResponseBody workOrder selectByPrimaryKey(){
+		return WorkOrderMapper.selectByPrimaryKey("112");
+	}
+}
