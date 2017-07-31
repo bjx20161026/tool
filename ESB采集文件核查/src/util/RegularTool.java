fff9b4136ef0fef98a19a22590f26460c1f4f913
BJX <1596311683@qq.com>
2017-07-31 09:03:39 +0800

    Change-Id: I671dce4d1dda601ed6c6e77fe24bee749ba1c4d3

---
+++ ESB采集文件核查/src/util/RegularTool.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularTool {
+	public static boolean IsNumber(String str){
+		String format = "^(-?\\d+)(\\.\\d+)?$";
+		 Pattern p = Pattern.compile(format,Pattern.CASE_INSENSITIVE|Pattern.MULTILINE);  
+	     Matcher matcher = p.matcher(str);  
+		return matcher.find();	
+	}
+	
+	public static void main(String[] args){
+		System.out.println(RegularTool.IsNumber("1234234.34"));
+	}
+
+}
